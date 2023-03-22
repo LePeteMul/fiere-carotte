f017028356214ffcd0174d7bb7f68493366209c3
Boivinos <boivinantonin@gmail.com>
2023-03-22 15:13:15 +0100

    menu comsetic effects on desktop + responsive fixed

--- nav.js
+++ nav.js
@@ -5,9 +5,46 @@ function getURL() {
     URL = URL.split("/")
     return URL[URL.length - 1]
 }
+let pagesNav = {}
 
-let pagesNav = {
-    index: `<ul>
+function setHTML() {
+    if (window.screen.width >= 675) {
+        pagesNav = {
+            index: `<ul>
+    <li><a href="./carte.html">Menu du restaurant</a></li>
+    <li><a href="./valeurs.html">Nos valeurs</a></li>
+    <li><a href="./nouscontacter.html">Nous contacter</a></li>   
+    <li><a href="./Reservation.html">Reserver</a></li>
+    </ul>`,
+            carte: ` <ul>
+    <li><a href="./index.html">Accueil</a></li>
+    <li><a href="./valeurs.html">Nos valeurs</a></li>
+    <li><a href="./nouscontacter.html">Nous contacter</a></li>
+    <li><a href="./Reservation.html">Reserver</a></li>
+    </ul>`,
+            valeurs: ` <ul>
+    <li><a href="./index.html">Accueil</a></li>
+    <li><a href="./carte.html">Menu du restaurant</a></li>
+    <li><a href="./nouscontacter.html">Nous contacter</a></li>
+    <li><a href="./Reservation.html">Reserver</a></li>
+    </ul>`,
+            contact: `<ul>
+    <li><a href="./index.html">Accueil</a></li>
+    <li><a href="./carte.html">Menu du restaurant</a></li>
+    <li><a href="./valeurs.html">Nos valeurs</a></li>
+    <li><a href="./Reservation.html">Reserver</a></li>
+</ul>`,
+            reservation: ` <ul>
+<li><a href="./index.html">Accueil</a></li>
+<li><a href="./valeurs.html">Nos valeurs</a></li>
+<li><a href="./nouscontacter.html">Nous contacter</a></li>
+<li><a href="./Reservation.html">Reserver</a></li>
+</ul>`}
+
+    } else {
+
+        pagesNav = {
+            index: `<ul>
     <li><a href="./carte.html">Menu du restaurant</a></li>
     <li><a href="./valeurs.html">Nos valeurs</a></li>
     <li><a href="./nouscontacter.html">Nous contacter</a></li>
@@ -20,7 +57,7 @@ let pagesNav = {
         </button>
     </li>
     </ul>`,
-    carte: ` <ul>
+            carte: ` <ul>
     <li><a href="./index.html">Accueil</a></li>
     <li><a href="./valeurs.html">Nos valeurs</a></li>
     <li><a href="./nouscontacter.html">Nous contacter</a></li>
@@ -31,7 +68,7 @@ let pagesNav = {
         </button>
     </li>
     </ul>`,
-    valeurs: ` <ul>
+            valeurs: ` <ul>
     <li><a href="./index.html">Accueil</a></li>
     <li><a href="./carte.html">Menu du restaurant</a></li>
     <li><a href="./nouscontacter.html">Nous contacter</a></li>
@@ -44,7 +81,7 @@ let pagesNav = {
         </button>
     </li>
     </ul>`,
-    contact: `<ul>
+            contact: `<ul>
     <li><a href="./index.html">Accueil</a></li>
     <li><a href="./carte.html">Menu du restaurant</a></li>
     <li><a href="./valeurs.html">Nos valeurs</a></li>
@@ -55,12 +92,10 @@ let pagesNav = {
         </button>
     </li>
 </ul>`,
-    reservation: ` <ul>
+            reservation: ` <ul>
 <li><a href="./index.html">Accueil</a></li>
 <li><a href="./valeurs.html">Nos valeurs</a></li>
 <li><a href="./nouscontacter.html">Nous contacter</a></li>
-
-
 <li>
     <button class="menu-reserver"><a href="./carte.html">
             <div>La Carte</div>
@@ -69,25 +104,36 @@ let pagesNav = {
 </li>
 </ul>`
 
+        }
+    }
 }
 
-switch (getURL()) {
-    case "index.html":
-        nav.innerHTML = pagesNav.index;
-        break;
-    case "carte.html":
-        nav.innerHTML = pagesNav.carte;
-        break;
-    case "valeurs.html":
-        nav.innerHTML = pagesNav.valeurs;
-        break;
-    case "nouscontacter.html":
-        nav.innerHTML = pagesNav.contact;
-        break;
-    case "Reservation.html":
-        nav.innerHTML = pagesNav.reservation;
-        break;
+function createNav() {
+    switch (getURL()) {
+        case "index.html":
+            nav.innerHTML = pagesNav.index;
+            break;
+        case "carte.html":
+            nav.innerHTML = pagesNav.carte;
+            break;
+        case "valeurs.html":
+            nav.innerHTML = pagesNav.valeurs;
+            break;
+        case "nouscontacter.html":
+            nav.innerHTML = pagesNav.contact;
+            break;
+        case "Reservation.html":
+            nav.innerHTML = pagesNav.reservation;
+            break;
+    }
+
 }
+
+setHTML()
+createNav()
+window.onresize = function () { setHTML(); createNav(); console.log("test") }
+window.addEventListener('resize', function () { setHTML(); createNav(); console.log("test") });
+
 
 
 
